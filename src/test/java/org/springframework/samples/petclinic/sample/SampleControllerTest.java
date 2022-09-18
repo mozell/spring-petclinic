e9f678f0a88440e02d6752bbe14d128040094730
mozell <mozellatto@gmail.com>
2022-09-18 15:39:22 +0900

    스프링 빈

--- src/test/java/org/springframework/samples/petclinic/sample/SampleControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/sample/SampleControllerTest.java
@@ -1,16 +1,33 @@
 package org.springframework.samples.petclinic.sample;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class SampleControllerTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleControllerTest {
+
+	@Autowired
+	ApplicationContext applicationContext;
 
 	@Test
-	public void testDoSomething() {
-		SampleRepository sampleRepository = new SampleRepository();
-		SampleController sampleController = new SampleController(sampleRepository);
-		sampleController.doSomething();
+	public void testDI() {
+		SampleController bean = applicationContext.getBean(SampleController.class);
+		assertThat(bean).isNotNull();
 	}
+
+//	@Test
+//	public void testDoSomething() {
+//		SampleRepository sampleRepository = new SampleRepository();
+//		SampleController sampleController = new SampleController(sampleRepository);
+//		sampleController.doSomething();
+//	}
 
 }
